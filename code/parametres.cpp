3e7a362b53b229ad25e4846f6cb2f46f7f13a391
Rémi Dupré <remim.dupre@gmail.com>
2015-07-23 15:47:43 +0200

    Equilibrage

--- code/parametres.cpp
+++ code/parametres.cpp
@@ -20,7 +20,7 @@ float SEUIL_LISSAGE = 1;
 float SEUIL_VARIANCE = 0.1;
 
 // Limite au dessus de laquelle on redécoupe la partie
-float SEUIL_DECOUPE = 49;
+float SEUIL_DECOUPE = 625;
 
 // Taille minimum de redécoupe
 int TAILLE_MIN_DECOUPE = 4;
