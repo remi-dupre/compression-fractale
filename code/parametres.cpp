ecab5d43505c476719a6f75f502992b7b7fc6c15
Rémi Dupré <remim.dupre@gmail.com>
2016-01-02 11:05:51 +0100

    Un seuil adapté

--- code/parametres.cpp
+++ code/parametres.cpp
@@ -20,7 +20,7 @@ float SEUIL_LISSAGE = 625;
 float SEUIL_VARIANCE = 625;
 
 // Limite au dessus de laquelle on redécoupe la partie
-float SEUIL_DECOUPE = 1000;
+float SEUIL_DECOUPE = 2000;
 
 // Taille minimum de redécoupe
 int TAILLE_MIN_DECOUPE = 4;
